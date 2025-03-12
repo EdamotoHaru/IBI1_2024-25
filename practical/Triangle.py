b00e9abdd3192d6f25103849f8ddce14f67c9430
EdamotoHaru <EdamotoHaru@163.com>
2025-03-12 18:28:51 +0800

    1

---
+++ practical/Triangle.py
@@ -0,0 +1,2 @@
+n = int(input('How many layers the triangle has?'))
+print("The T value for your triangle is", int(n*(n+1)/2))
---
+++ practical/variables.py
@@ -0,0 +1,9 @@
+a = 15
+b = 75
+c = a + b
+d = 90
+e = 5
+f = d + e
+print(d>f)
+X = True
+Y = False
