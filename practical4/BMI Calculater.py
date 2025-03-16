dd7b8eb1fdbddb2e53e5eba76786898aa519ad7c
EdamotoHaru <EdamotoHaru@163.com>
2025-03-16 12:33:00 +0800

    updated

--- practical4/BMI Calculater.py
+++ practical4/BMI Calculater.py
@@ -1,3 +1,9 @@
-weight = int(input('Enter your weight in kg: '))
+weight = float(input('Enter your weight in kg: '))
 height = float(input('Enter your height in cm: '))
-print('Your BMI is', weight/(height/100)**2)
+if weight/(height/100)**2 < 18.5:
+    a = 'Underweight'
+elif weight/(height/100)**2 >= 30:
+    a = 'Overweight'
+elif 18.5<= weight/(height/100)**2 < 30: 
+    a = 'Normal'
+print('Your BMI is',weight/(height/100)**2 , 'and you are', a)
