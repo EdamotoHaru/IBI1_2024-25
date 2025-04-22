e9cdae19599ddc6c7dd0e381020879ccec400c25
EdamotoHaru <EdamotoHaru@163.com>
2025-04-22 21:29:03 +0800

    Update Triangle and BMI Calculater

--- practical4/BMI Calculater.py
+++ practical4/BMI Calculater.py
@@ -1,3 +1,11 @@
+# BMI Calculator
+# First, ask the user for their weight in kg and height in cm (cm is normally used)
+# Then, calculate the BMI using the formula
+# BMI = weight(kg) / height(m)^2
+# Finally, print the BMI and the corresponding weight category
+# If the BMI is in the range of 18.5 to 30, print "Normal"
+# If the BMI is less than 18.5, print "Underweight"
+# If the BMI is greater than 30, print "Overweight"
 weight = float(input('Enter your weight in kg: '))
 height = float(input('Enter your height in cm: '))
 if weight/(height/100)**2 < 18.5:
--- practical4/Triangle.py
+++ practical4/Triangle.py
@@ -1,2 +1,8 @@
+# Ask the user for the number of layers in a triangle and calculate the T value
+# The T value is the sum of the first n natural numbers, which is given by the formula n(n+1)/2
+# Initially, Print the first ten values as examples for n =1, 2, ..., 10
+print("For example, the first ten values are:")
+for i in range(1, 11):
+    print("n =", i, "T value =", int(i*(i+1)/2))
 n = int(input('How many layers the triangle has?'))
 print("The T value for your triangle is", int(n*(n+1)/2))
